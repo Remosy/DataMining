ce8a20a26464ff24367d5c65e9dbf31b8cbcbd43
Remosy <remosy>
2017-09-18 23:01:12 +1000

    Updated lemmatizer & filter funcs.

--- Demo/main.py
+++ Demo/main.py
@@ -1,17 +1,32 @@
 from preprocess import PreProcess
 #from textMiner import TextMiner
 class TextMining():
-
-    def main(self):
+    def __init__(self):
         super().__init__()
-        folderPath = input("Folder Path:")
-        folderPath = "/Users/remosy/DataMining/Demo/textData"
-        self.preprocessor = PreProcess(folderPath)
+        self.folderPath = "/Users/remosy/DataMining/Demo/textData"
+        self.preProcess = PreProcess()
+
+        # without transforming string
+        self.lemmatized_1 = self.preProcess.load_data1(self.folderPath)
+
+        # transform lemmatized_list(3 generators) into a long string separate by space
+        self.lemmatized_2 = self.preProcess.load_data2(self.folderPath)
+
+        self.filterResults =  self.preProcess.cleanByFrequency(self.lemmatized_2)
         #self.textminer = TextMiner()
         self.displayData()
 
+
     def displayData(self):
-        print("Finished")
+        #for i in self.vectors[0]:
+            #print(i)
+        print("File 1 has: "+ str(sum(1 for x in (self.lemmatized_1[0]))))
+        print("File 2 has: " + str(sum(1 for x in (self.lemmatized_1[1]))))
+        print("File 3 has: " + str(sum(1 for x in (self.lemmatized_1[2])))+'\n')
+        print("All text has: " + str(len(self.lemmatized_2.split())))
+        print("After filter, the # of words(freq <= 5) has: "+ str(len(self.filterResults))+'\n')
+        print(self.filterResults)
+        print("Finished \n")
 
 if __name__ == '__main__':
-    TextMining().main()
+    textMining = TextMining()
--- Demo/preprocess.py
+++ Demo/preprocess.py
@@ -4,52 +4,58 @@ from os.path import join
 
 from nltk.corpus import stopwords as sw
 from nltk.corpus import wordnet as wn
-from nltk import wordpunct_tokenize
+from nltk import wordpunct_tokenize, word_tokenize
 from nltk import WordNetLemmatizer
-from nltk import sent_tokenize
+from nltk import FreqDist
 from nltk import pos_tag
 
 
 class PreProcess:
-    def __init__(self, folder_path, stopwords=None, punct=None,
+    def __init__(self,stopwords=None, punct=None,
                  lower=True, strip=True):
         self.lower = lower
         self.strip = strip
         self.stopwords = stopwords or set(sw.words('english'))
         self.punct = punct or set(string.punctuation)
         self.lemmatizer = WordNetLemmatizer()
-        rawSentences = []
-        self.load_data(folder_path)
-        # self.vectorData()
 
-    def load_data(self, folder_path):
+    def load_data1(self, folder_path):
+        collection = []
         for file in listdir(folder_path):
             if file.endswith(".txt") | file.endswith(".csv"):
                 path = join(folder_path, file)
-                self.process_doc(path)
-        return self
+                print("Start to process file: " + path, end='\n')
+                collection.append(self.process_doc(path))
+        return collection
 
-    def fit(self, X, y=None):
-        return self
+    def load_data2(self, folder_path):
+        collection = ""
+        for file in listdir(folder_path):
+            if file.endswith(".txt") | file.endswith(".csv"):
+                path = join(folder_path, file)
+                print("Start to process file: " + path, end='\n')
+                temp = " ".join(self.process_doc(path))
+                collection += (temp + " ")
+        return collection
 
-    def inverse_transform(self, X):
-        return [" ".join(doc) for doc in X]
-
-    def transform(self, X):
-        return [
-            list(self.tokenize(doc)) for doc in X
-        ]
-
-    def tokenize(self, document):
-        # Break the document into sentences
-        for sent in sent_tokenize(document):
-            # Break the sentence into part of speech tagged tokens
-            for token, tag in pos_tag(wordpunct_tokenize(sent)):
+    def process_doc(self, path):
+        #print("Start to process file: " + path, end='\n')
+        count = 0
+        f = open(path, 'r')
+        for line in f:
+            line = line.split('\t')
+            count = count + 1
+            #print("No. "+str(count)+": "+line[0], end='\n')
+            for token, tag in pos_tag(wordpunct_tokenize(line[0])):
                 # Apply preprocessing to the token
                 token = token.lower() if self.lower else token
                 token = token.strip() if self.strip else token
                 token = token.strip('_') if self.strip else token
                 token = token.strip('*') if self.strip else token
+                #token = token.strip('.') if self.strip else token
+                #token = token.strip(',') if self.strip else token
+
+                #print("[" + token + "____" + tag + " ] ")
 
                 # If stopword, ignore token and continue
                 if token in self.stopwords:
@@ -63,47 +69,13 @@ class PreProcess:
                 lemma = self.lemmatize(token, tag)
                 yield lemma
 
-    def process_doc(self, path):
-        print("Start to process file: " + path, end='\n')
-        count = 0
-        f = open(path, 'r')
-        for line in f:
-            line = line.split('\t')
-            count = count + 1
-            print("No. "+str(count)+": "+line[0], end='\n')
-            for token, tag in pos_tag(wordpunct_tokenize(line[0])):
-                # Apply preprocessing to the token
-                token = token.lower() if self.lower else token
-                token = token.strip() if self.strip else token
-                token = token.strip('_') if self.strip else token
-                token = token.strip('*') if self.strip else token
-                #token = token.strip('.') if self.strip else token
-                #token = token.strip(',') if self.strip else token
-
-                print("[" + token + "____" + tag + " ] ")
-
-                # If stopword, ignore token and continue
-                if token in self.stopwords:
-                    continue
-
-                # If punctuation, ignore token and continue
-                if all(char in self.punct for char in token):
-                    continue
-
-                # Lemmatize the token and yield
-                lemma = self.lemmatize(token, tag)
-                #yield lemma
-
-                for i in lemma:
-                    print("Lemmatize: " + i, end='\n')
-                print("--------------------------------")
-                if count > 3:
-                    print("***")
-                    return
-
-
-                
-
+                # Debug using
+                #for i in lemma:
+                    #print("Lemmatize: " + i, end='\n')
+                #print("--------------------------------")
+                #if count > 3:
+                    #print("***")
+                    #return
 
     def lemmatize(self, token, tag):
         tag = {
@@ -113,6 +85,15 @@ class PreProcess:
             'J': wn.ADJ
         }.get(tag[0], wn.NOUN)
         return self.lemmatizer.lemmatize(token, tag)
+
+    def transform(self, X):
+        return [" ".join(doc) for doc in X]
+
+    def cleanByFrequency(self,lemmatized_string):
+        #Filter by the frequency of appearance <= 5
+        token = word_tokenize(lemmatized_string)
+        distance = FreqDist(token)
+        return list(filter(lambda x: x[1] <= 5, distance.items()))
 
     def vector_Data(self, cleaned_Data):
         return
